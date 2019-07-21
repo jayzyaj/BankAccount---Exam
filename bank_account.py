86fc85e8956ad5166431c36b6f4572937cfe25e1
John Clifford de Vera <deverajaycee17@gmail.com>
2019-07-21 13:50:21 +0800

    Restructured test folder

--- bank_account.py
+++ bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
-  def __init__(self, name, accountName, accountNumber):
+  def __init__(self, name, accountName, accountNumber, initialDeposit = round(0, 2)):
     self.name = name
     self.accountName = accountName
     self.accountNumber = accountNumber
+    self.balance = initialDeposit
     self.overdraft = False
