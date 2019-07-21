b585d204f69ec8aaef5843f23a28083f08435e3c
John Clifford de Vera <deverajaycee17@gmail.com>
2019-07-21 18:58:54 +0800

    added some label

--- tests/test_helper_formatter.py
+++ tests/test_helper_formatter.py
@@ -17,6 +17,7 @@ class TestHelperFormatter(unittest.TestCase):
       print('tearDown\n')
 
     def test_should_return_correct_monetary_value(self):
+      print('Testing Helper / Formatter / Formatting of Correct monetary value')
       self.assertEqual(formatMoney(13.949999999999999), 13.95)
       self.assertEqual(formatMoney(14.22222223), 14.22)
       self.assertEqual(formatMoney(2.16), 2.16)
